311a04d79b1daedb66844ef312a95a8a6a780fc9
CChenxiiiii <a188086800192021@outlook.com>
2025-08-06 17:59:41 +0800

    Initial Commit

---
+++ CCToolBox/CCToolBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  CCToolBoxApp.swift
+//  CCToolBox
+//
+//  Created by chenxi on 2025/8/6.
+//
+
+import SwiftUI
+
+@main
+struct CCToolBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
